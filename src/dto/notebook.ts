d0ae9791e43575e010798995e20fd4e0036073d0
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-02-17 12:57:49 +0700

    add notebook DTO interfaces for API responses and requests

---
+++ src/dto/notebook.ts
@@ -0,0 +1,16 @@
+export interface GetAllNotebooksResponse {
+  id: string;
+  name: string;
+  parent_id: string | null;
+  created_at: Date;
+  updated_at: Date | null;
+}
+
+export interface CreateNotebookRequest {
+  name: string;
+  parent_id: string | null;
+}
+
+export interface CreateNotebookResponse {
+  id: string;
+}
