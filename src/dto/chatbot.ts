a5541da7d388bb4f7765868ccaeb2c9d1f224cbf
muhammadzhuhry <muhammadzhuhry@gmail.com>
2026-02-24 22:04:35 +0700

    add get history chat dto

--- src/dto/chatbot.ts
+++ src/dto/chatbot.ts
@@ -4,3 +4,10 @@ export interface GetAllSessionsResponse {
   createdAt: Date;
   updatedAt: Date | null;
 }
+
+export interface GetChatHistoryResponse {
+  id: string;
+  role: string;
+  chat: string;
+  createdAt: Date;
+}
